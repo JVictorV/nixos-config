0b1e3fa60f353eb827d857bc1adb2bad4d64a879
JVictorV <joaovictorvisona@gmail.com>
2021-06-06 01:28:13 -0300

    enabling docker

--- config/configuration.nix
+++ config/configuration.nix
@@ -26,6 +26,8 @@
     };
   };
 
+  virtualisation.docker.enable = true;
+
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
