729fb5e1d1a7cf77c68bd68b80e99c482a66f6a1
JVictorV <joaovictorvisona@gmail.com>
2021-10-13 12:22:19 -0300

    add an overlay to gitkraken

--- nixpkgs/overlays/index.nix
+++ nixpkgs/overlays/index.nix
@@ -7,4 +7,10 @@ self: super: {
     src = builtins.fetchTarball
       "https://github.com/dylanaraps/neofetch/archive/master.tar.gz";
   });
+  gitkraken = super.gitkraken.overrideAttrs (oldAttrs: {
+    src = self.fetchzip {
+      url = "https://release.axocdn.com/linux/GitKraken-v8.0.1.tar.gz";
+      sha256 = "1n88m41424qwsfp2hy58piqpv2dk6i74hcj184aq6njllvnsznnq";
+    };
+  });
 }
